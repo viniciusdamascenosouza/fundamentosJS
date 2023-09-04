ecaf89b3b301406586cd9e26f89968dac9db659b
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-04 17:10:12 -0300

    Template string

---
+++ type-string.js
@@ -0,0 +1,17 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'Olá, mundo!';
+
+const senha = "senhasuperseguro1234"
+
+const stringDeNumero = "34567";
+
+const citacao = 'Meu nome é '
+const nome = "Vinícius"
+
+console.log(citacao);
+
+// TEMPLATE STRING OU TEMPLATE LITERAL
+
+// CONCATENAÇÃO (+)
+
+console.log(citacao + nome)
