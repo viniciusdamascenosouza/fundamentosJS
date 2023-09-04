9ca439ffbd8fa10e7163af58797213266aa3edfa
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-04 16:48:31 -0300

    Ponto flutuante

---
+++ type-number.js
@@ -0,0 +1,25 @@
+const meuNumero = 3;
+
+const primeiroNumero = 10;
+
+const segundoNumero = 5;
+
+const primeiraSoma = primeiroNumero + segundoNumero;
+
+const primeiraSubtracao = primeiroNumero - segundoNumero;
+
+const primeiraMultiplicacao = primeiroNumero * segundoNumero;
+
+const primeiraDivisao = primeiroNumero / segundoNumero;
+
+console.log(primeiraSubtracao);
+
+// PONTO FLUTUANTE
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+// pode ser escrito dessa forma: ".5" sendo automaticamente = 0.5
+
+const operacoesComDecimal = pontoFlutuanteSemZero / Math.PI;
+console.log(operacoesComDecimal);
+
