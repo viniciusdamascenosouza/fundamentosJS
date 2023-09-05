5eda71150370ae6204df8e26f8fd9e320912f34d
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-05 16:53:32 -0300

    Anotações importantes

---
+++ JavaScriptAndNodeJS.js
@@ -0,0 +1,39 @@
+//  TIPAGEM DINÂMICA
+// pode ser reatribuido futuramente, o tipo de dado e o próprio dado
+let minhaVar = 567;
+
+minhaVar = "9000";
+
+console.log(typeof minhaVar);
+console.log(minhaVar);
+
+// MULTIPARADIGMA
+// pode resolver problemas de diversas formas,
+// pórem pode se resolver com a forma mais básica, tornando a curva de apredizado maior
+
+
+// porque chama JavaScript?
+// O JavaScript teve vários nomes. O primeiro foi Mocha, o segundo foi LiveScript, 
+// e quando a Netscape passou a ter suporte á tecnologia Java em seu navegador, 
+// mudou o nome para JavaScript, como um jogo de marketing, para popularizar o script.
+
+// O ECMAScript é a própria linguagem JavaScript e sua padronização.
+
+// O que é ES6 / ES2015
+// É a versão mais famosa do javascript, que trouxe importantes definições e atualizações por exemplo let e const...
+// não existiam nas versões anteriores
+
+// JavaScript é uma linguagem interpretada
+
+// LINGUAGEM COMPILADA X LINGUAGEM INTERPRETADA
+// Em uma linguagem compilada, a máquina de destino traduz o programa diretamente. 
+// Em uma linguagem interpretada, o código fonte não é traduzido diretamente pela máquina de destino. 
+// Em vez disso, um programa diferente, o interpretador, lê e executa o código.
+
+// NodeJS
+// Node.js é um software de código aberto, multiplataforma, baseado no interpretador V8 do Google 
+// e que permite a execução de códigos JavaScript fora de um navegador web. 
+// A principal característica do Node.js é sua arquitetura assíncrona e orientada por eventos
+
+// Por o JS ser uma linguagem interpretada, é necessário um interpretador, ele pode ser interpretado pelo navagador ou
+// podemos usar o NodeJS que será o JS no backend
