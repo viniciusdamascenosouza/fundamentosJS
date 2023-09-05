e1350cef44d43ea161160c47a49cde81f7277fbb
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-05 14:31:45 -0300

    var e let

---
+++ declarando-variaveis.js
@@ -0,0 +1,26 @@
+// var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+
+// console.log(area);
+
+// let
+let forma = 'retângulo';    
+let altura = 5;
+let comprimento =7;
+
+let area;
+
+if(forma === 'retângulo') {
+    area = altura * comprimento;
+} else {
+ area = (altura * comprimento) /2
+}
+
+area = altura * comprimento;
+
+console.log(area);
+
