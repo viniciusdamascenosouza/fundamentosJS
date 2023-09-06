28dfd65f77fa0574ad0dde9a423f80efb6b33f1e
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-06 08:12:31 -0300

    Operadores de comparações

---
+++ comparacoes.js
@@ -0,0 +1,22 @@
+// COMPARAÇÃO IMPLÍCITA
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+console.log(numero === texto);
+
+// typeof => mostra o tipo de dado
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == só compara o valor
+// === compara o valor e o tipo de dado
+
+// é mais usado como boas práticas ===
+
+// conversão explícita
+Number();
+String();
