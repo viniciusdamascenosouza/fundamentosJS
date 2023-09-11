e7a1e6cc245fc0d634a4bbf045b2a4cb35bf5ad2
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-11 17:06:59 -0300

    Operador ternário parte 1

---
+++ ternario.js
@@ -0,0 +1,8 @@
+const idadeMinima = 18;
+const idadeCliente1 = 19;
+
+if(idadeCliente1 >= idadeMinima) {
+    console.log("O cliente pode consumir bebida alcoólica!")
+} else {
+    console.log("O cliente é de menor, não pode consumir bebida alcoólica!")
+}
