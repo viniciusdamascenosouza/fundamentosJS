e5d6251966467e81c06dd3a89d17fcbf4c1a013f
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-12 18:10:16 -0300

    Observações sobre os arrays

--- arrays-media.js
+++ arrays-media.js
@@ -11,3 +11,10 @@ const notas1 = [10, 6.5, 8, 7.5];
 const media = (notas1[0] + notas1[1] + notas1[2] + notas1[3]) / notas1.length
 
 console.log(media);
+
+// cada dado armazenado em um array pode ser chamado de: elementos, itens e valores
+// esses itens são identificados por um índice ou posição que sempre começa do zero
+
+// os arrays são criados em const, let ou var 
+// recebem um nome, e seus dados ficam separados por vírgula dentro de colchetes: []
+// os arrays aceitam tipos diferentes dentro de uma mesma lista
