62083284a7bc0227831342836c5b5075769928fd
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-12 18:03:55 -0300

    A importância de arrays no JavaScript

---
+++ arrays-media.js
@@ -0,0 +1,13 @@
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+
+// const media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+// posições(índice):    0    1   2   3
+const notas1 = [10, 6.5, 8, 7.5];
+
+const media = (notas1[0] + notas1[1] + notas1[2] + notas1[3]) / notas1.length
+
+console.log(media);
