ca3e199378b3daa3e09a140dbb4b82e46c5aea35
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-12 16:49:24 -0300

    Arrow function parte 1

---
+++ arrow-function.js
@@ -0,0 +1,8 @@
+function apresentar(nome) {
+  return `Meu nome é ${nome}`;
+}
+
+const apresentarArrow = nome => `Meu nome é ${nome}!`;
+const soma = (num1, num2) => num1 + num2;
+
+console.log(soma(78, 22))
