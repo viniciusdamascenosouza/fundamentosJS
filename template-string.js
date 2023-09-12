a8adb1476ea37dcd0b018b74c27779812030bc23
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-12 11:06:33 -0300

    Concatenação comum

---
+++ template-string.js
@@ -0,0 +1,7 @@
+const nome = "Vinícius";
+const idade = 2023 - 2002;
+const cidadeDeNascimento = "Andrelândia-MG"
+
+const apresentacao = "Meu nome é " + nome + ", tenho " + idade + " anos e nasci na cidade de " + cidadeDeNascimento + ".";
+
+console.log(apresentacao);
