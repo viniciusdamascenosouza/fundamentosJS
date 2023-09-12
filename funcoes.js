689620e4e5bd4fc13a489e5f5dd66068ed29e22d
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-12 11:29:59 -0300

    Funções parte 1

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+   
+                       //string
+function imprimeTexto (texto) {
+    console.log(texto)
+}
+
+imprimeTexto("Sou o texto que a função irá imprimir!");
+imprimeTexto("Sou o texto secundário que a função irá exibir");
