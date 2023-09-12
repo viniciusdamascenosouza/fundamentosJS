d3e446d49db80c7ba05d387dd72e246c45a564ff
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-12 15:23:14 -0300

    Expreesões de função

---
+++ expressoes-funcao.js
@@ -0,0 +1,12 @@
+function minhaFuncao() {
+  // bloco de código
+}
+
+minhaFuncao("parametro");
+
+// expressão de função
+const soma = function (num1, num2) {
+  return num1 + num2;
+};
+
+console.log(soma(10, 5));
