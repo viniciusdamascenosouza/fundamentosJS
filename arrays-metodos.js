481f4d31620b12bc63e6179f2cfb85cd6675143b
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-13 08:02:44 -0300

    Método de array: .concat()

---
+++ arrays-metodos.js
@@ -0,0 +1,9 @@
+// .concat()
+// Este método concatena dois arrays, não altera os originais e resulta em um novo array concatenado
+const alunos = ["Luan", "João", "Marcos", "Estevão", "Lucas"];
+
+const alunas = ["Luara", "Stefanie", "Marcela", "Maria"];
+
+const alunosGeral = alunos.concat(alunas);
+
+console.log(alunosGeral);
