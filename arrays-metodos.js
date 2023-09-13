300fe1ac5779b4e9019763eac10f4c5471e9ba42
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-13 08:34:16 -0300

    Método filter

--- arrays-metodos.js
+++ arrays-metodos.js
@@ -1,9 +1,24 @@
 // .concat()
 // Este método concatena dois arrays, não altera os originais e resulta em um novo array concatenado
-const alunos = ["Luan", "João", "Marcos", "Estevão", "Lucas"];
+// const alunos = ["Luan", "João", "Marcos", "Estevão", "Lucas"];
 
-const alunas = ["Luara", "Stefanie", "Marcela", "Maria"];
+// const alunas = ["Luara", "Stefanie", "Marcela", "Maria"];
 
-const alunosGeral = alunos.concat(alunas);
+// const alunosGeral = alunos.concat(alunas);
 
-console.log(alunosGeral);
+// console.log(alunosGeral);
+
+// ----------------------------------------------------------------------------------------------
+
+
+// const alunos = ["Luan", "João", "Marcos", "Estevão", "Lucas"];
+
+// const alunosComNomesPequenos = alunos.filter((aluno) => aluno.length <= 6);
+
+// console.log(alunosComNomesPequenos);
+
+const DDDs = [32, 31, 43, 35, 24, 78, 38];
+
+const DDDsMG = DDDs.filter((ddd) => ddd >= 31);
+
+console.log(DDDsMG)
