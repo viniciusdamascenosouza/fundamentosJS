4bb04639cb7994f3c82c6b20ba3539de9f072ed7
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-13 17:15:38 -0300

    Loop infinito

--- for.js
+++ for.js
@@ -9,3 +9,8 @@ const numeros = [100, 200, 300, 400, 500, 600];
 for (let i = 0; i < numeros.length; i++) {
     console.log(numeros[i])
 }
+
+for (let i = 0; i <= 1;  i++) {
+    i--
+    console.log("teste!")
+}
