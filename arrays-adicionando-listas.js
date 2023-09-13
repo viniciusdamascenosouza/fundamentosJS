067c4452d39305ed81f97734f3f7e2b4f58db0aa
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-13 07:41:30 -0300

    Removendo elementos de uma lista

--- arrays-adicionando-listas.js
+++ arrays-adicionando-listas.js
@@ -1,7 +1,12 @@
- const notas = [10, 6, 8];
+// Adicionando elementos com .push()
+// O método push() adiciona um ou mais elementos ao final de um array e retorna o novo comprimento desse array.
+// dentros dos parenteses vão os elementos que serão adicionados a partir do último índice
+const notas = [10, 6, 8];
 
- notas.push(5);
+notas.push(5, 22, 332);
 
- const media = (notas[0] + notas [1] + notas[2] + notas[3]) / notas.length;
+console.log(notas);
 
- console.log(media);
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
---
+++ arrays-deletando-elementos.js
@@ -0,0 +1,12 @@
+// Deletando elementos com .pop()
+// O método pop() remove o último elemento de um array e retorna aquele elemento.
+const notas = [10, 6, 8, 5.5, 10];
+
+notas.pop(3);
+
+console.log(notas)
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+// console.log(`A média é ${media}.`)
+
