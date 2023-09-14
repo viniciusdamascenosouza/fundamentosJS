4e70b39a157aa91d1dde53c480e260328400cf5a
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-14 17:50:14 -0300

    Removendo elementos repetidos

--- Set.js
+++ Set.js
@@ -1,7 +1,10 @@
 const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
 
 const meuSet = new Set(nomes);
-
+meuSet.add("Vinícius", "Vinícius");
 const nomesAtualizados = [...meuSet];
+
+// tudo isso poderia ser subtituido por:
+// const nomesAtualizados = [...new Set(nomes)]
 
 console.log(nomesAtualizados);
