548eb1a87ddf78cc3a3132276d6fc7d2bf121e9e
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-14 17:40:01 -0300

    Clonando com spread operator

--- copiando-arrays.js
+++ copiando-arrays.js
@@ -1,7 +1,12 @@
 const notas = [7, 7, 8, 9];
 
-const novasNotas = notas;
+// ... = Esse é um operador do JavaScript que se chama spread operator, ou "operador de espalhamento". 
+// Ao utilizá-lo junto com uma lista, ele pega todo o conteúdo desse array e espalhar seus valores no local que 
+// estamos indicando. É como se copiássemos o conteúdo da lista, "7, 7, 8, 9", e colássemos no lugar do [...notas]. 
+// É isso que estamos dizendo para o JavaScript.
+const novasNotas = [...notas];
 
 novasNotas.push(10);
 
 console.log(`As novas notas são: ${novasNotas}`);
+console.log(`As novas notas são: ${notas}`);
