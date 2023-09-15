90e50820f9d4831c70fee7719fc17f13f2d393a6
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-15 18:00:19 -0300

    O que são objetos

---
+++ objeto.js
@@ -0,0 +1,13 @@
+// ARRAYS
+const listaCPFs = [1111, 22222, 333333];
+
+const informacoesPessoa = ["nome", "José", "idade", 32, "cpf", "12892812222"];
+
+
+const objetoPessoa = {
+  nome: "José",
+  idade: 32,
+  cpf: "11122233444",
+};
+
+console.log(objetoPessoa);
