fd144d1bb1db2c078ef6b0dac4dcebccf97bf8bc
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-18 14:04:49 -0300

    Acessando dados com colchetes

---
+++ acesso-colchete.js
@@ -0,0 +1,23 @@
+const cliente = {
+  nome: "Júlio",
+  idade: 29,
+  cpf: "1122223344",
+  email: "julin@dominio.com",
+};
+
+console.log(
+  `O nome do cliente é ${cliente["nome"]}, ${cliente["nome"]} tem ${
+    cliente["idade"]
+  } anos, seu email é ${
+    cliente["email"]
+  } e os três primeiros digitos do seu cpf são: ${cliente["cpf"].substring(
+    0,
+    3
+  )}.`
+);
+
+const chaves = ["nome", "idade", "cpf", "email", "altura"];
+
+chaves.forEach((chave) => {
+  console.log(`A chave ${chave} tem valor ${cliente[chave]}.`);
+});
