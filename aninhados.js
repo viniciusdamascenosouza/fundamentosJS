79f47e0f5d70c23d157b57e39a643eb32a9cfd5b
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-18 15:18:50 -0300

    Objetos em objetos

---
+++ aninhados.js
@@ -0,0 +1,15 @@
+const vendedor = {
+  nome: "Josué",
+  idade: "35",
+  telefone: "12221111",
+  email: "jusuedasvendas@dominio.com",
+};
+
+vendedor.endereco = {
+  rua: "Ibituruva",
+  bairro: "Amazonas",
+  numero: "221",
+  complemento: "Apartamento 9",
+};
+
+console.log(vendedor.endereco['bairro']);
