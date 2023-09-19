4ba61c893b87ac7d33a960e706709865eed0452f
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-09-19 08:11:49 -0300

    Sintaxe de espalhamento

--- chaves.js
+++ chaves.js
@@ -1,7 +1,7 @@
 const vendedor = {
   nome: "Josué",
   idade: "35",
-  telefone: "12221111",
+  telefone: ["122211", "9089999990"],
   email: "jusuedasvendas@dominio.com",
 };
 
@@ -18,6 +18,33 @@ vendedor.enderecos = [
 const chavesDoObjeto = Object.keys(vendedor);
 // console.log(chavesDoObjeto);
 
-if(!chavesDoObjeto.includes("endereco")){
-    console.error("Erro! É necessário ter um endereço cadastrado.")
-}
+// if (!chavesDoObjeto.includes("endereco")) {
+//   console.error("Erro! É necessário ter um endereço cadastrado.");
+// }
+
+// function ligaParaCliente(telefoneComercial, telefoneResidencial) {
+//   console.log(`Ligando para ${telefoneComercial}`);
+//   console.log(`Ligando para ${telefoneResidencial}`);
+// }
+
+// ligaParaCliente(vendedor.telefone[0], vendedor.telefone[1]);
+// LINHA ACIMA PODERIA SER ESCRITA DA FORMA ABAIXO
+// ligaParaCliente(...vendedor.telefone);
+
+const encomenda = {
+  destinatario: vendedor.nome,
+  rua: vendedor.enderecos[0].rua,
+  bairro: vendedor.enderecos[0].bairro,
+  numero: vendedor.enderecos[0].numero,
+  complemento: vendedor.enderecos[0].complemento,
+  apartamento: vendedor.enderecos[0].apartamento,
+};
+
+// O Objeto acima deve ser subtituido pelo código abaixo
+
+const encomendaCerta = {
+  destinatario: vendedor.nome,
+  ...vendedor.enderecos[0],
+};
+
+console.log(encomendaCerta);
